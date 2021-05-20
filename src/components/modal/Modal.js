7cb57162cbb73ea1760cfac7e366e834d3d9efde
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-05-20 12:14:33 +0200

    Push to test if form functionality works on deployed sites

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -5,16 +5,6 @@ import './modal.css';
 const Modal = ({ open, children, onClose }) => {
   if (!open) return null;
 
-  // const MODAL_STYLES = { 
-  //   position: 'fixed',
-  //   top: '50%',
-  //   left: '50%',
-  //   transform: 'translate(-50%, -50%)',
-  //   backgroundColor: '#FFF',
-  //   padding: '50px',
-  //   zIndex: 1000
-  // }
-
   const OVERLAY_STYLES = {
     position: 'fixed',
     top: 0,
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,10 +1,30 @@
 import React, { useState } from 'react';
 import Modal from '../modal/Modal';
 import { Link } from 'react-router-dom';
+import { host } from '../../config.js';
 import './nav.css';
 
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const [newMessage, setNewMessage] = useState({message: ''});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('message to send: ', newMessage);
+    console.log('current host: ', host);
+    await fetch(`${host}/notes`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newMessage)
+    });
+    setNewMessage({
+      message: ''
+    });
+  }
 
   return (
     <nav className="nav">
@@ -13,10 +33,10 @@ const Nav = () => {
       <Link to="/about"><button>About Inspirit</button></Link>
       <button className="button" onClick={() => setIsOpen(true)}>Add a note</button>
       <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-        <form>
+        <form onSubmit={handleSubmit}>
           <p>Write down something great about yourself in the box below</p>
           <p>(You can even paste a link to a video or pictures)</p>
-          <input type="text" name="message" />
+          <input type="text" name="newmessage" value={newMessage.message} onChange={(e) => setNewMessage({message: e.target.value})} />
           <button type="submit">Submit</button>
         </form>
       </Modal>
