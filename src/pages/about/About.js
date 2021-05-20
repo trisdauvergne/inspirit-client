57038d44e1cd11d95d5ceb1ddd72da706c52f81c
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-05-20 14:37:16 +0200

    Early styling of intro pages with fade in

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './about.css';
 
 const About = () => {
   return (
-    <section className="section about">
+    <section className="section">
       <Link to="/"><button>Back</button></Link>
+      <section className="intro-pages fade-in">
       <h1>About</h1>
       <h1>Inspirit</h1>
       <h3>verb (used with object)</h3>
       <h3>to infuse spirit or life into; enliven.</h3>
       <Link to="/instructions"><button>Instructions</button></Link>
+      </section>
     </section>
   )
 }
