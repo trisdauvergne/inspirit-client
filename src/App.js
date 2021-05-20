8945b553eae0862db6aac92934488f8659bf1ef8
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-05-20 09:02:13 +0200

    Removed some React boilerplate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { host } from './config.js';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -25,18 +24,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <p>{!data ? 'Nothing' : data.message}</p>
       </header>
     </div>
