3c742e9903842d80c14004a1059cced6e7c86bab
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-05-20 17:22:53 +0200

    Updated styling of the instructions page

--- src/pages/instructions/Instructions.js
+++ src/pages/instructions/Instructions.js
@@ -8,13 +8,13 @@ const Instructions = () => {
       <Link to="/about">
         <button>Back to about</button>
       </Link>
-      <section className="intro-pages extra-spacing">
-        <h1 className="sub-heading fade-in">Use Inspirit when imposter syndrome is creeping in</h1>
-        <div className="slower-fade-in">
-          <h3>to remind yourself of your accomplishments...</h3>
-          <h3>to reflect over what you're capable of...</h3>
-          <h3>to immerse yourself in positivity...</h3>
-          <h3>(or to just look at a few things that make you smile)</h3>
+      <section className="intro-pages intro-pages--less-margin">
+        <h1 className="heading instructions__heading fade-in">When to use<br/>INSPIRIT</h1>
+        <div className="instructions__div slower-fade-in">
+          <h3 className="instruction__txt">when impostor syndrome creeps in...</h3>
+          <h3 className="instruction__txt">to remind yourself of your accomplishments...</h3>
+          <h3 className="instruction__txt">to reflect over what you're capable of...</h3>
+          <h3 className="instruction__txt instruction__txt--last">to immerse yourself in positivity...</h3>
           <Link to="/notes">
             <button className="standard-btn">Notes</button>
           </Link>
