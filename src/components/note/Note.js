03b061c54667339b6810d8defdcc75601c554460
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-05-21 14:29:31 +0200

    Improved method for putting in videos

--- src/components/note/Note.js
+++ src/components/note/Note.js
@@ -4,7 +4,7 @@ import './note.css';
 const Note = ({ note }) => {
   return (
     <section className="note fade-in">
-      {note.message ? <p className="note__txt">{note.message}</p>: <iframe className="note__video" title="Inspirit Video" width="200" height="120" src={note.video} frameBorder="0"></iframe>}
+      {note.message ? <p className="note__txt">{note.message}</p>: <iframe className="note__video" title="Inspirit Video" width="200" height="120" src={`https://www.youtube.com/embed/${note.video}`} frameBorder="0"></iframe>}
     </section>
   )
 }
